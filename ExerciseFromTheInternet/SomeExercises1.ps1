904019898c5f76d79c88f5518a898b5442c8c20e
Jastrzębski <tymon.jastrzebski@t-systems.com.pl>
2023-08-10 12:03:31 +0200

    Third wave of scripts

---
+++ ExerciseFromTheInternet/SomeExercises1.ps1
@@ -0,0 +1,19 @@
+﻿Clear-Host
+
+Get-Service | Select-Object -Property Name,Status | Where-Object {$_.Status -eq "Stopped"}
+
+Write-Output `n
+
+Get-Service | Select-Object -Property Name | Where-Object {$_.Name -Like "A*"}
+
+Write-Output `n
+
+Get-Service | Select-Object -Property Name | Where-Object {$_.StartType -eq "Automatic"}
+
+Write-Output `n
+
+#Restart-Service -Name Winmgmt
+
+Get-Service | Select-Object -Property Name | Out-File "$PSScriptRoot\services.txt"
+
+Get-Service | Select-Object -Property Name,Status,StartType | ConvertTo-Html | Out-File "$PSScriptRoot\services.html"
---
+++ ExerciseFromTheInternet/SomeExercises2.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+
+Get-Process | Where-Object {$_.Name -like "A*"}
+
+Write-Output `n
+
+Get-Process | Where-Object {$_.Name -eq "svchost" -and $_.PM -gt 10000}
